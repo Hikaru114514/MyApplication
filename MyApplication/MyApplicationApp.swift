a14cb468cdab52b2608f996b2dcae3baba0d3dea
Hikaru114514 <cutomer114@gmail.com>
2025-04-23 12:36:12 +0900

    Initial Commit

---
+++ MyApplication/MyApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyApplicationApp.swift
+//  MyApplication
+//
+//  Created by tamuralab on 2025/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MyApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
